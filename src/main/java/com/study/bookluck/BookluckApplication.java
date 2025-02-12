59c5d1312d00345b57379eb2c02b5b7fbff3f052
geunwon-kim <gwgw424@gmail.com>
2025-02-12 20:02:41 +0900

    cors error fix

--- src/main/java/com/study/bookluck/BookluckApplication.java
+++ src/main/java/com/study/bookluck/BookluckApplication.java
@@ -2,6 +2,9 @@ package com.study.bookluck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BookluckApplication {
@@ -9,5 +12,15 @@ public class BookluckApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookluckApplication.class, args);
 	}
+
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://front-server.com");
+            }
+        };
+    }
 
 }
